31c5a9ff818185d5a952f2855767c63ffbe3674b
Chris Rowles <42321445+chrisdevelops@users.noreply.github.com>
2018-11-01 12:20:26 +0000

    Update centos-7.sh

--- provisioning/centos-7.sh
+++ provisioning/centos-7.sh
@@ -40,12 +40,11 @@ httpd -v
 touch /etc/yum.repos.d/mariadb.repo
 cat <<EOM >/etc/yum.repos.d/mariadb.repo
 [mariadb]
-name = MariaDB
-baseurl = http://yum.mariadb.org/10.2/centos7-amd64
+name=MariaDB
+baseurl=http://yum.mariadb.org/10.2/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOM
-rpm --import 
 yum -y install mariadb-server
 systemctl start mariadb
 systemctl enable mariadb
